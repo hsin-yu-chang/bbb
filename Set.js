905c3a6652cc16ca2e24c105f093f4a28eb2662d
hsin-yu-chang <73873533+hsin-yu-chang@users.noreply.github.com>
2021-01-12 14:19:08 +0800

    Update Set.js

--- Set.js
+++ Set.js
@@ -6,7 +6,7 @@ function open()
 {
 	var content = document.getElementById("content");
 	var l = document.getElementById('letter');
-	l.setAttribute( "src", "letop.jpg" ); 
+	l.innerHTML="<img src='letop.jpg' width:'90%'>"
 	content.innerHTML = "";
 	content.innerHTML += '<p>Dear 親愛的使用者:</br></br>'+'已經有多久沒寫信問候親朋好友了呢？</br></br>'+'是時候該動動滑鼠，</br></br>'+'寫信給親愛的朋友了吧！</p></br>';
 	document.getElementById('letter').addEventListener("click", close, false);
